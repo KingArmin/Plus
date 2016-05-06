784aee3a5a504fce26c5e5319a064711a758febb
Armin <armin.bagheri.1380@gmail.com>
2016-05-06 08:35:59 +0430

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -5,7 +5,7 @@ URL = require('socket.url')
 JSON = require('dkjson')
 HTTPS = require('ssl.https')
 ----config----
-local bot_api_key = ""
+local bot_api_key = "220861604:AAFrCYZVedojp4OqXBRLbXO4Bpk-_4O74Es"
 local BASE_URL = "https://api.telegram.org/bot"..bot_api_key
 local BASE_FOLDER = ""
 local start = [[HI
@@ -46,7 +46,7 @@ _just send a sticker_
 *photo to sticker*
 _just send a photo_
 
-[Source](https://github.com/pAyDaAr/lua-api-bot) ;-)
+[Channel](https://Telegram.me/TeleIranTeam)
 ]] 
 
 -------
